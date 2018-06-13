5444591bbbdc5e1e2f0f3cd0ac9b2bf2ddddfd5f
ameet.srivastava <ameet.srivastava@markit.com>
2018-06-13 15:17:46 +0530

    Test Class

---
+++ comdemodocker/src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ameet.srivastava on 6/13/2018.
+ */
+public class Hello {
+
+    public static void main(String args[])
+    {
+        System.out.println("This is my first git created hello world program");
+    }
+}
